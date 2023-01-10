9458d7330a72fce00cb8944b5c985c5b2fbed96e
therealpaulgg <paul@paul.systems>
2023-01-09 23:43:51 -0700

    delete

--- pkg/web/router/routes/machine.go
+++ pkg/web/router/routes/machine.go
@@ -1,11 +1,44 @@
 package routes
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/samber/do"
+	"github.com/therealpaulgg/ssh-sync-server/pkg/database/models"
+	"github.com/therealpaulgg/ssh-sync-server/pkg/web/middleware"
 )
+
+type DeleteRequest struct {
+	MachineName string `json:"machine_name"`
+}
 
 func MachineRoutes(i *do.Injector) chi.Router {
 	r := chi.NewRouter()
+	r.Use(middleware.ConfigureAuth(i))
+	r.Delete("/", func(w http.ResponseWriter, r *http.Request) {
+		user, ok := r.Context().Value(middleware.UserContextKey).(*models.User)
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		var deleteRequest DeleteRequest
+		if err := json.NewDecoder(r.Body).Decode(&deleteRequest); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		machine := models.Machine{}
+		machine.Name = deleteRequest.MachineName
+		machine.UserID = user.ID
+		if err := machine.GetMachineByNameAndUser(i); err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		if err := machine.DeleteMachine(i); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
 	return r
 }
