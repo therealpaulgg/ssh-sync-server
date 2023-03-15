48bffc7668b2661d9b391112e06badcd67c7fdc1
Paul Gellai <paul.gellai@carvana.com>
2023-03-14 23:25:28 -0700

    init

--- pkg/web/router/routes/machine.go
+++ pkg/web/router/routes/machine.go
@@ -7,10 +7,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 	"github.com/samber/do"
+	"github.com/samber/lo"
 	"github.com/therealpaulgg/ssh-sync-server/pkg/database/models"
 	"github.com/therealpaulgg/ssh-sync-server/pkg/web/middleware"
+	"github.com/therealpaulgg/ssh-sync/pkg/dto"
 )
 
 type DeleteRequest struct {
@@ -20,6 +23,53 @@ type DeleteRequest struct {
 func MachineRoutes(i *do.Injector) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.ConfigureAuth(i))
+	r.Get("/{machineId}", func(w http.ResponseWriter, r *http.Request) {
+		user, ok := r.Context().Value(middleware.UserContextKey).(*models.User)
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		err := user.GetUserMachines(i)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		machineId, err := uuid.Parse(chi.URLParam(r, "machineId"))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		machine, found := lo.Find(user.Machines, func(machine models.Machine) bool {
+			return machine.ID == machineId
+		})
+		if !found {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		machineDto := dto.MachineDto{
+			Name: machine.Name,
+		}
+		json.NewEncoder(w).Encode(machineDto)
+	})
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		user, ok := r.Context().Value(middleware.UserContextKey).(*models.User)
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		err := user.GetUserMachines(i)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		machineDtos := make([]dto.MachineDto, len(user.Machines))
+		for i, machine := range user.Machines {
+			machineDtos[i] = dto.MachineDto{
+				Name: machine.Name,
+			}
+		}
+		json.NewEncoder(w).Encode(machineDtos)
+	})
 	r.Delete("/", func(w http.ResponseWriter, r *http.Request) {
 		user, ok := r.Context().Value(middleware.UserContextKey).(*models.User)
 		if !ok {
